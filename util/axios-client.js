a9edcb3b4787d4b66a863fa13422f0fe17ca83e9
ju-liechen <juliechen4649@gmail.com>
2024-11-07 14:44:30 -0400

    Update axios baseUrl

--- util/axios-client.js
+++ util/axios-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosClient = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {'Content-Type': 'application/json'},
 })
