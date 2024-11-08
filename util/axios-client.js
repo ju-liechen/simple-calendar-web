a5f2e5218f43443f55f598a502dccddb772ab405
ju-liechen <juliechen4649@gmail.com>
2024-11-08 08:57:59 -0400

    Remove unused variable in axios-client.js

--- util/axios-client.js
+++ util/axios-client.js
@@ -13,10 +13,6 @@ axiosClient.interceptors.request.use(
     // Before sending the request, this function checks if the request URL does not start with '/public'.
     // If so, it retrieves the session data to add an Authorization header with the token to the request
     if (config.url) {
-      // Extract the path from config.url, accounting for both absolute and relative URLs
-      const urlPath = new URL(config.url, process.env.NEXT_PUBLIC_API_URL)
-        .pathname
-
       if (config.url && !config.url.startsWith('/api')) {
         config.baseURL = process.env.NEXT_PUBLIC_API_URL
       }
