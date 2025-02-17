1a75a2034d561791866a4ae538537697d4b7f86f
ju-liechen <juliechen4649@gmail.com>
2025-02-16 23:17:45 -0400

    Add event info to EventDetails

--- components/event-card/index.js
+++ components/event-card/index.js
@@ -1,15 +1,25 @@
+import { useStore } from 'util/store'
+
 import styles from './event-card.module.scss'
+
 
 export const EventCard = ({ data }) => {
   const { title, startDateTime, endDateTime } = data
+  const setModal = useStore((state) => state.setModal)
+  const setModalState = useStore((state) => state.setModalState)
 
   const formattedStartTime = new Intl.DateTimeFormat('en-US', {
     hour: 'numeric',
     hour12: true,
   }).format(new Date(startDateTime))
 
+  const onClickEventCard = () => {
+    setModalState(data)
+    setModal('EventDetails')
+  }
+
   return (
-    <div className={styles['event-card-wrapper']}>
+    <div className={styles['event-card-wrapper']} onClick={onClickEventCard}>
       <span className={styles['title']}>{title}</span>
       <span className={styles['start-time']}>{formattedStartTime}</span>
     </div>
--- components/modal/event-details.js
+++ components/modal/event-details.js
@@ -10,6 +10,18 @@ import styles from './event-details.module.scss'
 
 export function EventDetails() {
   const setModal = useStore((state) => state.setModal)
+  const eventData = useStore((state) => state.modalState)
+
+  const formattedStartTime = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    hour12: true,
+  }).format(new Date(eventData.startDateTime))
+
+  const formattedEndTime = new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    hour12: true,
+  }).format(new Date(eventData.endDateTime))
+
 
   function closeModal() {
     setModal(null)
@@ -27,12 +39,11 @@ export function EventDetails() {
     <Modal variant="small white" onClose={closeModal}>
       <Form className={styles['content-wrapper']} methods={methods}>
         <h2 className={classnames(['modal-title', styles.title])}>
-          Event Details
+          {eventData.title}
         </h2>
         <div className={styles.body}>
-          <div className={styles['select-wrapper']}>
-            Info here
-          </div>
+          <span>{formattedStartTime}</span>
+          <span>{formattedEndTime}</span>
         </div>
         <div className={styles.actions}>
           <SubmitButton fullWidth disabled={false}>
